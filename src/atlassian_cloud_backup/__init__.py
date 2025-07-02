478a4400f6111174180df3ceba85011c0bb50375
Timo Jakob <timojakob@icloud.com>
2025-07-02 22:38:53 +0200

    added sbom and set version to 0.1.0

--- src/atlassian_cloud_backup/__init__.py
+++ src/atlassian_cloud_backup/__init__.py
@@ -4,6 +4,6 @@ Atlassian Cloud Backup Tool
 A utility for automated backup of Jira and Confluence cloud instances.
 """
 
-__version__ = '1.0.0'
+__version__ = '0.1.0'
 
 from atlassian_cloud_backup.backup_controller import BackupController
